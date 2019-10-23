92e14819b89218a706270033ef75cd1d1ff7d983
escobar55 <escobar5@umbc.edu>
2019-10-23 11:16:54 -0400

    working dice

--- src/MonopolyDice.java
+++ src/MonopolyDice.java
@@ -6,21 +6,37 @@ public class MonopolyDice {
         Random r = new Random();
 
         int x = 1 + r.nextInt(6);
-
         int y = 1 + r.nextInt(6);
-
         int sum = x + y;
 
-        if ( x != y) {
-            System.out.println("User rolls " + x + " & " + y);
-            System.out.println("Move " + sum + " spaces and stop");
-        }
-        else if(x == y) {
-            System.out.println("User rolls " + x + " & " + y);
-            System.out.println("DOUBLES!");
-            System.out.println("Move " + sum + " spaces and roll again getting...");
+
+        int rolls = 0;
+        while(rolls < 3) {
+            if (x != y) {
+                System.out.println("User rolls " + x + " & " + y);
+                System.out.println("Move " + sum + " spaces and stop");
+                break;
+            }
+            else if (x == y ){
+                rolls++;
+                if (rolls == 3){
+                    System.out.println("User rolls " + x + " & " + y);
+                    System.out.println("DOUBLES!");
+                    System.out.println("GO TO JAIL --->");
+                }
+                else {
+                    System.out.println("User rolls " + x + " & " + y);
+                    x = 1 + r.nextInt(6);
+                    y = 1 + r.nextInt(6);
+                    System.out.println("DOUBLES!");
+                    System.out.println("Move " + sum + " spaces and roll again getting...");
+                    sum = x + y;
+                }
+            }
+
 
         }
+
 
 
     }
